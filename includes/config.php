834baffce9b6bddd3ef0c5f58d9cc1470a7be8ab
KatowProject <naufalfaqih443@gmail.com>
2020-11-17 18:57:54 +0700

    first

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') or die("No access direct allowed");
+$config = array(
+    'webhookURL' => "", //Wenhook URL
+    'username' => "", //username webhook
+    'avatarWebhook' => "", //avatar webhook
+);
+?>
---
+++ menu.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') or die("No Access Allowed");?>
+<div class="content">
+    <div class="inner">
+		
+	<img src="https://cdn.discordapp.com/attachments/496983030993518592/778208372520255508/POSBG-SLCCC.png">		
+	<h1>Sedang Mencari Staff</h1>
+		<h4>Jawab Pertanyaan ini sejujujurnya tidak perlu khawatir dengan privasi karna ini sifatnya rahasia</h4>
+	</div>
+</div>
+	<nav>
+		<ul>
+			<li><a href="#instagram" onClick="">Instagram</a></li>
+			<li><a href="#facebook" onClick="">Facebook</a></li>
+			<li><a href="#discord" onClick="">Discord</a></li>
+				<!--<li><a href="#elements">Elements</a></li>-->
+		</ul>
+	</nav>
